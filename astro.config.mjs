2943071c6fdddf700e578e1c7cc1e8baad5ed1da
Luishiño <pericena12@gmail.com>
2024-06-18 15:07:16 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import aws from 'astro-sst';
 import netlify from '@astrojs/netlify';
-// https://astro.build/config
+
 export default defineConfig({
-  // site: 'https://pericena.github.io/',
-  // base: 'presupuesto.io',
-  site: 'https://presupuestosw2.netlify.app',
-  sitemap: true,
-  outDir: 'dist', // Asegúrate de que esto sea correcto
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: netlify(),
-  // adapter: aws({
-  //   deploymentStrategy: "edge",
-  // }),
+  integrations: [
+    react(),
+    tailwind()
+  ],
+  output: {
+    format: 'static', // Generar archivos estáticos
+  },
+  adapter: netlify(), // Adaptador para despliegue en Netlify
 });
-
-
